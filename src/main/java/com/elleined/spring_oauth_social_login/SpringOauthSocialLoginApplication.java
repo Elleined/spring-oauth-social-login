2ae742323c66c63989880d08c6986e318e081369
Elleined <demadegu@gmail.com>
2024-06-03 08:35:09 +0800

    Initial commit

---
+++ src/main/java/com/elleined/spring_oauth_social_login/SpringOauthSocialLoginApplication.java
@@ -0,0 +1,13 @@
+package com.elleined.spring_oauth_social_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthSocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauthSocialLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elleined/spring_oauth_social_login/SpringOauthSocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elleined.spring_oauth_social_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthSocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
