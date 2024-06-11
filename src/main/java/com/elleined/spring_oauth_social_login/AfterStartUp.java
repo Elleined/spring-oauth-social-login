09da01725dcbe774276c340f2e28968984a1b37d
Elleined <demadegu@gmail.com>
2024-06-11 13:00:36 +0800

    Added datafaker

--- src/main/java/com/elleined/spring_oauth_social_login/AfterStartUp.java
+++ src/main/java/com/elleined/spring_oauth_social_login/AfterStartUp.java
@@ -1,21 +1,33 @@
 package com.elleined.spring_oauth_social_login;
 
+import com.elleined.spring_oauth_social_login.repository.DBUserRepository;
 import com.elleined.spring_oauth_social_login.service.db.DBUserService;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.datafaker.Faker;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AfterStartUp {
 
+    private final Faker faker;
+
+    private final DBUserRepository dbUserRepository;
     private final DBUserService userService;
 
     @PostConstruct
     void addDefaultUsers() {
-        if (userService.isEmailAlreadyExists("sample@gmail.com"))
+        if (dbUserRepository.existsById(1))
             return;
 
-        userService.save("sample@gmail.com", "sample", "Sample Name", "Sample Image");
+        log.debug("Saving pre-defined users! Please wait...");
+        String email = faker.bothify("????#??@gmail.com");
+        String name = faker.name().fullName();
+        String image = faker.avatar().image();
+        userService.save(email, "password", name, image);
+        log.debug("Saving pre-defined users! Success...");
     }
 }
--- src/main/java/com/elleined/spring_oauth_social_login/SpringOauthSocialLoginApplication.java
+++ src/main/java/com/elleined/spring_oauth_social_login/SpringOauthSocialLoginApplication.java
@@ -1,13 +1,19 @@
 package com.elleined.spring_oauth_social_login;
 
+import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringOauthSocialLoginApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(SpringOauthSocialLoginApplication.class, args);
 	}
 
+	@Bean
+	public Faker faker() {
+		return new Faker();
+	}
 }
